ec0212f96402526ce53051296b13cfff547bbfaa
Juliancio Carvalho <juliancio_alves@hotmail.com>
2021-03-23 22:26:01 -0300

    feat: category

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { Category } from "../model/Category";
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+  const category = new Category();
+
+  return response.status(201).json({ category });
+});
+
+export { categoriesRoutes };
