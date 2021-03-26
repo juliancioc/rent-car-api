4c9e0bc50692ad453f41775271131a4989b35ed7
Juliancio Carvalho <juliancio_alves@hotmail.com>
2021-03-25 21:06:40 -0300

    feat: create category

--- src/repositories/ICategoriesRepository.ts
+++ src/repositories/ICategoriesRepository.ts
@@ -1,7 +1,13 @@
 import { Category } from "../model/Category";
 
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
 interface ICategoriesRepository {
   findByName(name: string): Category;
   list(): Category[];
-  crerate(name: string, description): void;
+  create({ name, description }: ICreateCategoryDTO): void;
 }
+
+export { ICategoriesRepository, ICreateCategoryDTO };
